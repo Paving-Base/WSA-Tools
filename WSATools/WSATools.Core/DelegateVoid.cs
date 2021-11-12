f0966e33e9f05b941ded6b5a8700b3369f5cc3be
wherewhere <qq2518766683@outlook.com>
2021-11-12 18:06:30 +0800

    删除内容

--- WSATools/WSATools.Core/DelegateVoid.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Windows;
-
-namespace WSATools.Core
-{
-    public delegate void CloseHandler(object sender, bool? result);
-    public delegate void EnableHandler(object sender, bool state);
-    public delegate void LoadingHandler(object sender, Visibility result);
-}
